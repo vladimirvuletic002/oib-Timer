9a807a464f97c3b2b7b7a4b7d7f25444ff56e970
vladimirvuletic002 <vladimirvuletic12@gmail.com>
2024-12-16 22:25:23 +0100

    Autentifikacija

--- Timer/TimerService/TimerService.cs
+++ Timer/TimerService/TimerService.cs
@@ -11,40 +11,10 @@ namespace TimerService
 {
     public class TimerService : ITimerService
     {
-        public static Dictionary<string, User> UserAccountsDB = new Dictionary<string, User>();
-
-        /// <summary>
-        /// Add new user to UserAccountsDB. Dictionary Key is "username"
-        /// </summary>
-        public void AddUser(string username, string password)
+        public void TestCommunication()
         {
-            if (!UserAccountsDB.ContainsKey(username))
-            {
-                UserAccountsDB.Add(username, new User(username, password));
-            }
-            else
-            {
-                Console.WriteLine($"Korisnik sa korisnickim imenom {username} vec postoji u bazi");
-            }
-
-            IIdentity identity = Thread.CurrentPrincipal.Identity;
-
-            Console.WriteLine("Tip autentifikacije : " + identity.AuthenticationType);
-
-            WindowsIdentity windowsIdentity = identity as WindowsIdentity;
-
-            Console.WriteLine("Ime klijenta koji je pozvao metodu : " + windowsIdentity.Name);
-            Console.WriteLine("Jedinstveni identifikator : " + windowsIdentity.User);
-
-            Console.WriteLine("Grupe korisnika:");
-            foreach (IdentityReference group in windowsIdentity.Groups)
-            {
-                SecurityIdentifier sid = (SecurityIdentifier)group.Translate(typeof(SecurityIdentifier));
-                string name = (sid.Translate(typeof(NTAccount))).ToString();
-                Console.WriteLine(name);
-            }
+            Console.WriteLine("Communication established.");
         }
-
 
         public void StartTimer()
         {
